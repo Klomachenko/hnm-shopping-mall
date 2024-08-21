888194ab253f636d27a954bcc7348f1367cb5c08
Klomachenko <leekyumin0901@naver.com>
2024-08-21 23:17:04 +0900

    feat: product api call 구현

---
+++ src/component/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://noona-hnm.netlify.app/ankle-jeans.jpeg' alt='' />
+      <div>Conscious Choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>$99900</div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
-  return <div>ProductAll</div>;
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products/`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
 };
 
 export default ProductAll;
