77cbd55b0a49aaead842c816455a3485b4ae7f42
Klomachenko <leekyumin0901@naver.com>
2024-08-22 15:36:23 +0900

    feat: useParams 통한 상품 개별 데이터 가져오기

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  let { id } = useParams();
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  );
 };
 
 export default ProductDetail;
